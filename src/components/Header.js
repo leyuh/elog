cdf5b4fda3555235b40508be05d37a4758de9558
Claire Devlin <clairebeargoldielox@icloud.com>
2022-11-19 10:58:36 -0800

    begin header

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/Header.css';
+import logo from '../images/elon1.png';
+import tesla from '../images/tesla.png';
+import spaceX from '../images/spaceX.png';
+import twitter from '../images/twitter.png';
+import boringComp from '../images/boringComp.png';
+import neuralink from '../images/neuralink.png';
+
+function Header (props) {
+    return <div id="header">
+        <h1 id="header-title-1">the</h1>
+        <h1 id="header-title-2">EL{<img id="header-logo" src={logo}/>}G</h1>
+        <ul id="header-list">
+            <li><img class="list-img" src={tesla}/></li>
+            <li><img class="list-img" src={spaceX}/></li>
+            <li><img class="list-img" src={twitter}/></li>
+            <li><img class="list-img" src={boringComp}/></li>
+            <li><img class="list-img" src={neuralink}/></li>
+        </ul>
+    </div>
+}
+
+export default Header;
