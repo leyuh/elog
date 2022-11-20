c20437a9e980183c1d903a162e508c3c856560f7
Claire Devlin <clairebeargoldielox@icloud.com>
2022-11-20 10:58:44 -0800

    Update header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './styles/App.css';
 import Header from './components/Header';
 import HomePage from './components/pages/HomePage';
+import React, {useState} from 'react';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(<HomePage/>);
+  
   return (
     <div className="App">
       <Header/>
-      <HomePage/>
+      {currentPage}
     </div>
   );
 }
