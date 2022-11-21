8285e5cf198941e75a1e8007a437759839ff502f
Claire Devlin <clairebeargoldielox@icloud.com>
2022-11-21 11:36:02 -0800

    Start wiki component

---
+++ src/components/apiComponents/Wiki.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import logo from "../../images/wikiLogo.png";
+import "../../styles/Wiki.css";
+
+function Wiki (props) {
+    const {wikiPageTitle} = props;
+
+    let eWikiPageTitle = "";
+
+    for (let char in wikiPageTitle) {
+        if (wikiPageTitle[char] != "_") {
+            eWikiPageTitle += wikiPageTitle[char];
+        } else {
+            eWikiPageTitle += " ";
+        }
+    }
+
+    const [wiki, setWiki] = useState(null);
+
+    useEffect(() => {
+        const url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=" + wikiPageTitle;
+
+        fetch(url)
+            .then((response) => response.json())
+            .then(data => {
+                setWiki(Object.values(data.query.pages)[0].extract);
+            })
+            .catch(err => console.log(err));
+
+    }, [])
+
+    return <div className="wikiDiv">
+        <h3 className="wikiTitle">{eWikiPageTitle}</h3>
+        <hr/>
+        <p className="wikiParagraph">{wiki}</p>
+        <a className="wikiLink" target="_blank" href={`https://en.wikipedia.org/wiki/${wikiPageTitle}`}>Read more</a>
+        <img className="wikiImg" src={logo}/>
+    </div>
+}
+
+export default Wiki;
--- src/components/pages/Tesla.js
+++ src/components/pages/Tesla.js
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
+import Wiki from "../apiComponents/Wiki";
 
 import '../../styles/Tesla.css';
 
 function Tesla (props) {
-
-    const [wiki, setWiki] = useState(null);
-
-    let key = "AAAAAAAAAAAAAAAAAAAAADKjjQEAAAAANeCr7nDNfszFl3KlobPFLplpOSM%3D3WL02OR2W89dRbOVwuXNxgjEnL3zzYST93dhmjXHyHvVQosfqV";
-
-    useEffect(() => {
-        console.log("ping");
-        var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=Elon_Musk";
-
-        fetch(url)
-            .then((response) => response.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-
-    }, [])
-
     return <div id="tesla-page" className="main-page">
-        <div></div>
+        <Wiki wikiPageTitle="Tesla,_Inc."/>
     </div>
 }
 
