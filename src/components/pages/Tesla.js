8fff639f37ffb1fd85773cebd8e6944fdc1ead97
Claire Devlin <clairebeargoldielox@icloud.com>
2022-11-25 09:51:49 -0800

    Begin Tweets comp

--- src/components/pages/Tesla.js
+++ src/components/pages/Tesla.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Wiki from "../apiComponents/Wiki";
 
 import '../../styles/Tesla.css';
 
 function Tesla (props) {
+
+    let token = "AAAAAAAAAAAAAAAAAAAAADKjjQEAAAAANeCr7nDNfszFl3KlobPFLplpOSM%3D3WL02OR2W89dRbOVwuXNxgjEnL3zzYST93dhmjXHyHvVQosfqV"
+
+    useEffect(() => {
+        const url = "https://api.twitter.com/2/tweets/search/recent?query=from:elonmusk";
+
+        fetch(url, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+            .then((response) => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.log(err));
+
+    }, [])
+
     return <div id="tesla-page" className="main-page">
         <Wiki wikiPageTitle="Tesla,_Inc."/>
     </div>
